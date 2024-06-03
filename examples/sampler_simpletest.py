071c7cc2bfc93f4b0d7a17c46da62338f0d6f208
ThomasPDye <tompdye@googlemail.com>
2024-06-03 11:32:21 +0100

    first full commit with untested example

--- examples/sampler_simpletest.py
+++ examples/sampler_simpletest.py
@@ -2,3 +2,31 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024 Thomas Dye
 #
 # SPDX-License-Identifier: Unlicense
+import time
+import board
+from analogio import AnalogIn
+from math import ceil
+
+from sampler import VoltageSampler
+
+sample_period = 0.1     # minimum time between samples
+print_period = 10       # minimum time between prints
+sleep_period = 0.01     # target time to sleep between time comparisons (approximately equivalent to timing precision)
+
+num_samples = ceil(print_period / sample_period)
+
+battery_voltage_sampler = VoltageSampler(AnalogIn(board.VOLTAGE_MONITOR), ratio = 2.0, max_samples = num_samples)
+
+last_print_time = last_sample_time = time.monotonic()
+battery_voltage_sampler.update()
+print(battery_voltage_sampler.average())
+
+while True:
+    t = time.monotonic()
+    if t - last_sample_time >= sample_period:
+        battery_voltage_sampler.update()
+        last_sample_time = t
+    if t - last_print_time >= print_period:
+        print("{:.2f} V".format(battery_voltage_sampler.average(num_samples)))
+        last_print_time = t
+    time.sleep(sleep_period)
--- sampler.py
+++ sampler.py
@@ -23,15 +23,51 @@ Implementation Notes
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://circuitpython.org/downloads
-
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
-  based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
 
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/ThomasPDye/CircuitPython_Sampler.git"
+
+class Sampler:
+    def __init__(self, max_samples: int = 256):
+        self.__max_samples = max_samples
+        self.__samples = []
+
+    def __get(self):
+        pass
+
+    def len(self):
+        return len(self.__samples)
+
+    def update(self):
+        while self.len() >= self.__max_samples:
+            self.__samples.pop(0)
+        self.__samples.append(self.__get())
+
+    def average(self, lastn: int = 128):
+        if self.len() == 0:
+            self.update()
+        N = self.len()
+        startn = 0
+        if 0 < lastn and lastn <= N:
+            startn = N - lastn
+        sum = 0
+        for n in range(startn,N):
+            sum += self.__samples[n]
+        return sum / (N - startn)
+
+    def reset(self):
+        self.__samples.clear()
+
+from analogio import AnalogIn
+
+class VoltageSampler(Sampler):
+    def __init__(self, ain: AnalogIn, ratio: float = 1.0, max_samples:int = 1024):
+        super().__init__(max_samples)
+        self.__ain = ain
+        self.__ratio = ratio
+
+    def __get(self):
+        return self.__ratio * (self.__ain.value * self.__ain.reference_voltage) / 65536
